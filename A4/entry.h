2c6fe395f8af6c47f8e86f41cfff592d1394ff9b
jeyan <justin_yan_2@sfu.ca>
2021-01-07 04:01:43 -0800

    Upload New File

---
+++ A4/entry.h
@@ -0,0 +1,24 @@
+#ifndef _ENTRY_H_
+#define _ENTRY_H_
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Entry{
+    private:
+        int key;
+        string data;
+    public:
+        Entry();            //constructors
+        ~Entry();           //deconstructor
+        void random();      //gives a random key btwn 0 - 99 inclusive, and a
+                            //random data string of 3 lowercase letters
+        int getKey();       //get Key value
+        string getData();   //get Data value
+        string toString();  //converts entry to a string, ex: (14,drn)
+};
+
+#endif
