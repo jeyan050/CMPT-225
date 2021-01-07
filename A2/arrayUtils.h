c0bdbd6d819217f87f2786a98f8fa88ba778de56
jeyan <justin_yan_2@sfu.ca>
2021-01-07 03:59:05 -0800

    Upload New File

---
+++ A2/arrayUtils.h
@@ -0,0 +1,12 @@
+#ifndef _ARRAYUTILS_H_
+#define _ARRAYUTILS_H_
+
+template <typename T>
+class ArrayUtils{       //Class ArrayUtils with function declared
+    public:
+         T* reverse(T *usedArray, int size);
+};
+
+#include "arrayUtils.cpp"
+
+#endif
